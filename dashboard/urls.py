224aeafc045c0522d95d3e65795315191093daa2
Shivam Bhilare <bhilareshivam0741@gmail.com>
2025-12-06 21:09:21 +0530

    Budget Generator Done

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from dashboard.views import dashboard_views
 from dashboard.views import transactions_views
 from dashboard.views import addTransaction_views
+from dashboard.views import budgetGen_views
 app_name = 'dashboard'
 
 urlpatterns = [
@@ -11,4 +12,5 @@ urlpatterns = [
     path('process-voice/', addTransaction_views.process_voice_input, name='process_voice'),
     path('process-image/', addTransaction_views.process_image_input, name='process_image'),
     path('save-confirmed/', addTransaction_views.save_confirmed_transactions, name='save_confirmed'),
+    path('budget-generator/', budgetGen_views.budget_generator_view, name='budget_generator'),
 ]
