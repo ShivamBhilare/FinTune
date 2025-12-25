07ef1e3255f661248b9c7410b76b07760053595f
technerdsreenath <sreenathnair411@gmail.com>
2025-12-25 15:41:09 +0530

    Profile layout

--- Features/urls.py
+++ Features/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import dashboard_views
 
+app_name = 'dashboard'
+
 urlpatterns = [
-    path('', dashboard_views.dashboard_view, name='dashboard'),
+    path('', dashboard_views.dashboard_view, name='home'),
+    path('dashboard/', dashboard_views.dashboard_view, name='dashboard_main'),
+    path('profile/', dashboard_views.profile_view, name='profile'),
 ]
