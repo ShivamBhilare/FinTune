1caa4388513d457900762cf6ba3edb5a43cc4bbf
technerdsreenath <sreenathnair411@gmail.com>
2025-12-25 13:04:07 +0530

    Dashboard layout added

--- Features/urls.py
+++ Features/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import dashboard_views
 
 urlpatterns = [
-    path('', dashboard_views.home_redirect_view, name='home'),
-    path('dashboard/', dashboard_views.dashboard_view, name='dashboard'),
+    path('', dashboard_views.dashboard_view, name='dashboard'),
 ]
