a7e6ab2c01549ee99a4eec0edf8ca86d4289dbd9
Supriya Gupta <supriyagpt22@gmail.com>
2025-12-25 19:24:17 +0530

    Pattern Detection Feature Minor Modification

--- Features/urls.py
+++ Features/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import dashboard_views, addTransaction_views, bugetGenerator_views
+from .views import dashboard_views, addTransaction_views, bugetGenerator_views, pattern_views
 
 app_name = 'dashboard'
 
